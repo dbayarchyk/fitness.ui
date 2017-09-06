b9fa0f0006462ba7d20f8491936baf36f1fced9c
Dmitry Boyarchik <dima97boyarchik@gmail.com>
2017-09-06 13:37:52 +0300

    Add possibility to select user trainingPlan

--- src/components/TrainingPlans/TrainingPlans.js
+++ src/components/TrainingPlans/TrainingPlans.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import { gql, graphql } from 'react-apollo';
+import { connect } from 'react-redux';
 import {
   Col,
   Row
 } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
 
 import Spinner from '../common/Spinner/Spinner';
 import TrainingPlanCard from './components/TrainingPlanCard/TrainingPlanCard';
 
-const trainingPlans = gql`
-  query {
+const trainingPlansAndUser = gql`
+  query trainingPlansAndUser($userId: ID!){
     trainingPlans {
       _id,
       name,
       avatarUrl,
+      complexity,
       trainings {
         _id,
         date
+      }
+    },
+    user(_id: $userId) {
+      trainingPlan {
+        _id
       }
     }
   }
 `;
 
+const updateUser = gql`
+  mutation updateUser($id: ID!, $data: UserInput!) {
+    updateUser(_id: $id, data: $data) {
+      _id
+    }
+  }
+`;
+
 class TrainingPlans extends Component {
+  changeUserTrainingPlan = trainingPlan => this.props.updateUser({
+    variables: {
+      id: this.props.userId,
+      data: { trainingPlan, }
+    }
+  })
+    .then(data => this.props.data.refetch());
+
   render() {
     if (this.props.data.loading) {
       return <Spinner isLoading={this.props.data.loading} />
@@ -37,6 +61,11 @@ class TrainingPlans extends Component {
                 <Col xs="12" sm="12" md="3" lg="4" key={trainingPlan._id}>
                   <TrainingPlanCard 
                     trainingPlan={trainingPlan}
+                    titleRightIcon={
+                      !!this.props.data.user.trainingPlan && this.props.data.user.trainingPlan._id === trainingPlan._id 
+                        ? <FontAwesome name="minus-square" onClick={() => this.changeUserTrainingPlan(null)}/> 
+                        : <FontAwesome name="plus-square" onClick={() => this.changeUserTrainingPlan(trainingPlan._id)}/>
+                    }
                   />
                 </Col>
               ))
@@ -48,6 +77,19 @@ class TrainingPlans extends Component {
   }
 }
 
-const TrainingPlansWithData = graphql(trainingPlans)(TrainingPlans);
+const TrainingPlansWithData = graphql(trainingPlansAndUser, {
+  options: ({ userId }) => ({
+    variables: {
+      userId,
+    },
+    fetchPolicy: 'network-only'
+  })
+})(TrainingPlans);
 
-export default TrainingPlansWithData;
+const FoodPlansWithDataAndMutation = graphql(updateUser, { name: 'updateUser' })(TrainingPlansWithData);
+
+const mapStateToProps = state => ({
+  userId: state.auth.currentUser._id
+});
+
+export default connect(mapStateToProps)(FoodPlansWithDataAndMutation);
